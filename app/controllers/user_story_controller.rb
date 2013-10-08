59db0a304ba9287f9628a1d896a8c9f44e615aa2
pawelrychlik <prychlik@future-processing.com>
2013-10-08 20:46:00 +0200

    added role attributes

--- app/controllers/user_story_controller.rb
+++ app/controllers/user_story_controller.rb
@@ -1,18 +1,22 @@
 class UserStoryController < ApplicationController
 
   def initialize
-    @roles = ['User', 'Product Owner', 'Administrator']
+    @attributes = %w(hesitant irregular disguised corrupted handsome)
+    @roles = ['User', 'Product Owner', 'Administrator', 'Client Application']
     @goals = ['log in', 'see the monthly report', 'share my screen', 'call the operator', 'watch the stars']
     @benefits = ['they can help me out', 'I can sleep well tonight', 'I keep track of my money']
   end
 
   def index
-    @role = @roles.sample
-    @goal = @goals.sample
-    @benefit = @benefits.sample
+    @user_story = {
+      attribute: @attributes.sample,
+      role: @roles.sample,
+      goal: @goals.sample,
+      benefit: @benefits.sample
+    }
 
     respond_to do |format|
-      format.text { render text: "As a #{@role}, I can #{@goal} so that #{@benefit}." }
+      format.text { render text: "As a #{@user_story[:attribute]} #{@user_story[:role]}, I can #{@user_story[:goal]} so that #{@user_story[:benefit]}." }
       format.html { render layout: 'application' }
     end
   end
