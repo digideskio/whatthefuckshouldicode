91914e715a6dca5c802686f651345077da3123e2
pawelrychlik <prychlik@future-processing.com>
2013-10-08 16:18:22 +0200

    rails scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whatthefuckshouldicode::Application.config.secret_key_base = '7beca1d8f88d5e03c6938ac82745dba94c3446dc5335e6e320ee0c9214171d8b00f9618f37e6bd4e5015769022539fd13daf1feb06e0afea75d958936520dc34'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Whatthefuckshouldicode::Application.config.session_store :cookie_store, key: '_whatthefuckshouldicode_session'
