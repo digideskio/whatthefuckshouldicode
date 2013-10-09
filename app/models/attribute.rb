b2ffc67ed0f5fa372de8a6b1d5c72ea67072ddbb
pawelrychlik <prychlik@future-processing.com>
2013-10-09 18:32:14 +0200

    restructured attributes so that its easy to contribute

--- app/models/attribute.rb
+++ app/models/attribute.rb
@@ -1,27 +1,100 @@
 class Attribute
 
   @@attributes = %w(
-    admin active acquired anonymous associated automated authenticated authorized
-    blocked backup business-critical
-    cautious careless complaining corporate corrupted
-    dangerous decent deleted demanding deprecated direct disguised dishonest draft dummy
-    evil executive exotic external
-    fake faulty flaky flawless foreign
+    admin
+    active
+    acquired
+    anonymous
+    associated
+    automated
+    authenticated
+    authorized
+    blocked
+    backup
+    business-critical
+    cautious
+    careless
+    complaining
+    corporate
+    corrupted
+    dangerous
+    decent
+    deleted
+    demanding
+    deprecated
+    direct
+    disguised
+    dishonest
+    draft
+    dummy
+    evil
+    executive
+    exotic
+    external
+    fake
+    faulty
+    flaky
+    flawless
+    foreign
     generic
-    handsome harmful hesitant hidden highlighted
-    imaginary impolite important incoming incompatible independent internal irregular
-    key known
-    lame leading local
-    mentioned messy modern
+    handsome
+    harmful
+    hesitant
+    hidden
+    highlighted
+    imaginary
+    impolite
+    important
+    incoming
+    incompatible
+    independent
+    internal
+    irregular
+    key
+    known
+    lame
+    leading
+    local
+    mentioned
+    messy
+    modern
     new
-    old-fashioned ordinary
-    physical plain pointless potential premium private profitable
-    quarantined questionable quirky
-    random reckless referenced referred regular returning
-    secondary shared signed-in sophisticated stale stateless statistical stray
-    tagged third-party troubled true trusted
-    unknown unsupported useless
-    valid virtual
+    old-fashioned
+    ordinary
+    physical
+    plain
+    pointless
+    potential
+    premium
+    private
+    profitable
+    quarantined
+    questionable
+    quirky
+    random
+    reckless
+    referenced
+    referred
+    regular
+    returning
+    secondary
+    shared
+    signed-in
+    sophisticated
+    stale
+    stateless
+    statistical
+    stray
+    tagged
+    third-party
+    troubled
+    true
+    trusted
+    unknown
+    unsupported
+    useless
+    valid
+    virtual
     worst
   )
 
